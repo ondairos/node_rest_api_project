660094079f5d3bf56e1c7e6225797b178f9d7b02
Ondairos <ondairos@gmail.com>
2022-11-06 15:11:13 +0200

    added controller model

---
+++ controllers/users.js
@@ -0,0 +1,53 @@
+import { v4 as uuidv4 } from 'uuid'; //creates unique ids package
+
+let users = [];  //let declaration here
+
+export const createUser = (req, res) => {
+    const user = req.body;
+    const userId = uuidv4();
+    const userWithId = { ...user, id: userId }; //spread creation of objects
+
+    users.push(userWithId);
+
+    res.send(`User with the name ${user.firstName} added to the database.`);
+}
+
+
+export const getUsers = (req, res) => {
+    res.send(users);
+}
+
+export const getUser = (req, res) => {
+    const { id } = req.params; //destruct
+    const foundUser = users.find((user) => user.id === id);
+    res.send(foundUser);
+}
+
+export const deleteUser = (req, res) => {
+    const { id } = req.params;
+    users = users.filter((user) => user.id != id); //filter removes elements to false values
+    res.send(`User with the id ${id} deleted from the database.`);
+}
+
+
+export const updateUser = (req, res) => {
+    const { id } = req.params;
+    const { firstName, lastName, age } = req.body;
+
+    const userToBeUpdated = users.find((user) => user.id === id);
+
+    if (firstName) {
+        userToBeUpdated.firstName = firstName;
+    };
+
+    if (lastName) {
+        userToBeUpdated.lastName = lastName;
+    };
+
+    if (age) {
+        userToBeUpdated.age = age;
+    };
+
+    res.send(`User with the id ${id} has been updated.`)
+
+}
--- routes/users.js
+++ routes/users.js
@@ -1,65 +1,22 @@
 import express from 'express';
-import { v4 as uuidv4 } from 'uuid'; //creates unique ids package
+
+import { createUser, getUsers, getUser, deleteUser, updateUser } from '../controllers/users.js';
 
 const router = express.Router();
 
-let users = [];  //let declaration here
 
 //all routes here start with /users
-router.get('/', (req, res) => {
-    console.log(users);
+router.get('/', getUsers);
 
-    res.send(users);
-});
-
-
-router.post('/', (req, res) => {
-    const user = req.body;
-    const userId = uuidv4();
-    const userWithId = { ...user, id: userId }; //spread creation of objects
-
-    users.push(userWithId);
-
-    res.send(`User with the name ${user.firstName} added to the database.`);
-});
-
+router.post('/', createUser);
 
 //req.parms {id: }
-router.get('/:id', (req, res) => {
-    const { id } = req.params; //destruct
-    const foundUser = users.find((user) => user.id === id);
-    res.send(foundUser);
-});
+router.get('/:id', getUser);
 
 //delete users
-router.delete('/:id', (req, res) => {
-    const { id } = req.params;
-
-    users = users.filter((user) => user.id != id); //filter removes elements to false values
-    res.send(`User with the id ${id} deleted from the database.`);
-});
+router.delete('/:id', deleteUser);
 
 //update without overwrite (put overwrites)
-router.patch('/:id', (req, res) => {
-    const { id } = req.params;
-    const { firstName, lastName, age } = req.body;
-
-    const userToBeUpdated = users.find((user) => user.id === id);
-
-    if (firstName) {
-        userToBeUpdated.firstName = firstName;
-    };
-
-    if (lastName) {
-        userToBeUpdated.lastName = lastName;
-    };
-
-    if (age) {
-        userToBeUpdated.age = age;
-    };
-
-    res.send(`User with the id ${id} has been updated.`)
-
-})
+router.patch('/:id', updateUser);
 
 export default router;
